801415b0e93e61b4f8fb075affcfb0d3c1c65015
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-06-12 11:52:38 -0500

    viewing favorite topics

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Feed from '../components/Feed';
+import { Container, Header, Segment } from 'semantic-ui-react';
 import PageHeader from '../components/PageHeader';
 
 function Home() {
   return (
   <div>
     <PageHeader headTitle='TweetWatch'/>
-    <Feed />
+    <Container fluid className='home-page-about-container'>
+    <Segment circular clearing>
+      <Header as='h2'>
+        Get Started!
+      </Header>
+      </Segment>
+      <Segment className='home-page-about-content' textAlign='left'>
+      <p> <b>Welcome</b> to TweetWatch the sole purpose of this app is to filter the clutter of Twitter and provide you with your desired news, information, and topics as quickly and efficiently as possible.</p>
+    <p>
+      df
+    </p>
+      </Segment>
+  </Container>
   </div>
   );
 }
