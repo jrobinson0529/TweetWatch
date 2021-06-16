732b4fb89cdb99d0e9e54fdaa6bc255ae0613da0
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-06-15 20:24:14 -0500

    saving tweets and removing them

--- src/helpers/data/savedTweetsData.js
+++ src/helpers/data/savedTweetsData.js
@@ -7,5 +7,22 @@ const getSavedTweets = (uid) => new Promise((resolve, reject) => {
     .then((response) => resolve(Object.values(response.data)))
     .catch((error) => reject(error));
 });
-
-export default getSavedTweets;
+const saveTweet = (uid, tweetObject) => new Promise((resolve, reject) => {
+  getSavedTweets(uid).then((response) => {
+    const checkExistingTweet = response.filter((tweet) => tweet.tweetId === tweetObject.tweetId);
+    if (checkExistingTweet.length === 0) {
+      axios.post(`${dbUrl}/savedTweets.json`, tweetObject)
+        .then((res) => {
+          const body = { id: res.data.name };
+          axios.patch(`${dbUrl}/savedTweets/${res.data.name}.json`, body);
+        }).then(() => resolve(true));
+    }
+  }).catch((error) => reject(error));
+});
+const deleteSavedTweet = (uid, id) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/savedTweets/${id}.json`)
+    .then(() => {
+      getSavedTweets(uid).then(resolve);
+    }).catch((error) => reject(error));
+});
+export { saveTweet, getSavedTweets, deleteSavedTweet };
